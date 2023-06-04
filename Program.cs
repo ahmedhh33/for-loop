be873f21d923894cf766cf4ef5d5c2fa4cf8522d
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-04 12:07:34 +0400

    wileloop

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml;
+
+namespace for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <5; i++)
+            {
+
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            for(int i = 0; i <3;i++)
+            {
+                Console.WriteLine("*   *");
+            }
+            for(int i = 0; i <5;i++)
+            {
+                Console.Write("*");
+            }
+            int a = 0;
+            while (a<5)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+        }
+    }
+}
